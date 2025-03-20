462dd4fe483bc8bf200694c8cf10a27c283d0f47
Jussivan <jussivanbm1029@gmail.com>
2025-03-20 11:16:00 -0300

    add Modals

--- front/src/components/taskCard/taskCard.tsx
+++ front/src/components/taskCard/taskCard.tsx
@@ -1,8 +1,14 @@
 import { RiPencilFill } from "react-icons/ri";
 import { FaTrashAlt } from "react-icons/fa";
+import DeleteTask from "../modals/deleteTask/deleteTask";
+import EditTask from "../modals/editTask/editTask";
+import { useState } from "react";
 
 
 function TaskCard() {
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [editModal, setEditModal] = useState(false);
+
     return (
       <div className="bg-white border border-gray-200 rounded-lg shadow-md">
         {/*Título da Tarefa*/}
@@ -17,13 +23,15 @@ function TaskCard() {
         </div>
 
         <div className="flex justify-end px-6 pb-4 gap-5">
-            <button className="flex bg-green-600 py-2 px-4 rounded-lg items-center gap-1 text-white cursor-pointer">
+            <button className="flex bg-green-600 py-2 px-4 rounded-lg items-center gap-1 text-white cursor-pointer" onClick={() => {setEditModal(true)}}>
                 <RiPencilFill size={20}/>
                 <span className="font-semibold">Editar</span>
+                <EditTask isOpen={editModal}/>
             </button>
-            <button className="flex bg-red-600 py-2 px-4 rounded-lg items-center gap-1 text-white cursor-pointer">
+            <button className="flex bg-red-600 py-2 px-4 rounded-lg items-center gap-1 text-white cursor-pointer" onClick={() => {setDeleteModal(true)}}>
                 <FaTrashAlt/>
                 <span className="font-semibold">Deletar</span>
+                <DeleteTask isOpen={deleteModal}/>
             </button>
         </div>
       </div>
